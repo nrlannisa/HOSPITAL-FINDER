87c2787dd0924a2179ce3da05e914dfc367688db
nrlannisa <2021868368@student.uitm.edu.my>
2022-02-05 23:49:52 +0800

    new

---
+++ Hospital_Finder/app/src/main/java/com/example/hospitalfinder/MapsActivity.java
@@ -0,0 +1,94 @@
+package com.example.hospitalfinder;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MapsActivity extends FragmentActivity{
+
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        //initialize fused Location
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        //Check permission
+        if (ActivityCompat.checkSelfPermission(MapsActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            //get current location when permission is granted
+            getCurrentLocation();
+        } else {
+            //request permission when permission is denied
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+
+    private void getCurrentLocation() {
+        //Initialize task location
+
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                //When success
+                if (location != null) {
+                    //sync map
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            //Initialize Lat Lng
+                            LatLng latLng = new LatLng(location.getLatitude(),
+                                    location.getLongitude());
+                            //Create marker options
+                            MarkerOptions options = new MarkerOptions()
+                                    .position(latLng)
+                                    .title("I am here");
+                            //zoom map scale 15
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                            googleMap.addMarker(options);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                //Get current location when permission granted
+                getCurrentLocation();
+            }
+        }
+    }
+
+
+}
